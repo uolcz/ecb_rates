474d9e1ab884898d0a391d7e33463dfe236f886c
Pavel Hronek <hronek.pav@seznam.cz>
2019-04-04 14:32:25 +0200

    Added full history exchange rates

--- spec/ecb_rates/exchange_rates_spec.rb
+++ spec/ecb_rates/exchange_rates_spec.rb
@@ -3,10 +3,29 @@ require 'webmock/rspec'
 require 'timecop'
 
 RSpec.describe EcbRates::ExchangeRates do
+  before do
+    stub_request(:get, (EcbRates::TODAY_RATES)).
+      to_return(status: 200, body: load_today_fixture)
+
+    stub_request(:get, (EcbRates::HISTORY_RATES)).
+      to_return(status: 200, body: load_history_fixture)
+
+    stub_request(:get, (EcbRates::FULL_HISTORY_RATES)).
+      to_return(status: 200, body: load_full_history_fixture)
+
+    Timecop.freeze('2015-10-19')
+  end
+
+  after do
+    Timecop.return
+  end
+
   let(:today_rates)   { EcbRates::ExchangeRates.new(EcbRates::TODAY_RATES) }
   let(:history_rates) { EcbRates::ExchangeRates.new(EcbRates::HISTORY_RATES) }
+  let(:full_history_rates) { EcbRates::FullHistoryExchangeRates.new(EcbRates::FULL_HISTORY_RATES) }
 
   describe '#initialize' do
+
     context 'today' do
       it 'sets exchange rates today url' do
         expect(today_rates.url).to eq(EcbRates::TODAY_RATES)
@@ -18,23 +37,15 @@ RSpec.describe EcbRates::ExchangeRates do
         expect(history_rates.url).to eq(EcbRates::HISTORY_RATES)
       end
     end
+
+    context 'full_history' do
+      it 'sets exchange rates full history url' do
+        expect(full_history_rates.url).to eq(EcbRates::FULL_HISTORY_RATES)
+      end
+    end
   end
 
   describe '#exchange_rate_for' do
-    before do
-      stub_request(:get, (EcbRates::TODAY_RATES)).
-        to_return(status: 200, body: load_today_fixture)
-
-      stub_request(:get, (EcbRates::HISTORY_RATES)).
-        to_return(status: 200, body: load_history_fixture)
-
-      Timecop.freeze('2015-10-19')
-    end
-
-    after do
-      Timecop.return
-    end
-
     context 'today' do
       it 'returns exchange rate' do
         expect(today_rates.exchange_rate_for('JPY', Date.today)).to eq(135.29)
@@ -56,19 +67,17 @@ RSpec.describe EcbRates::ExchangeRates do
           to eq(nil)
       end
     end
-  end
 
-  # source url:
-  #
-  # http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
-  def load_today_fixture
-    File.open('spec/fixtures/eurofxref-daily.xml')
-  end
+    context 'full history' do
+      it 'returns exchange rate' do
+        expect(full_history_rates.exchange_rate_for('AUD', Date.today - 111)).
+          to eq(1.455)
+      end
 
-  # source url:
-  #
-  # http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
-  def load_history_fixture
-    File.open('spec/fixtures/eurofxref-hist-90d.xml')
+      it 'returns nil if exchange rates arent present for particular date' do
+        expect(full_history_rates.exchange_rate_for('AUD', Date.today - 1)).
+          to eq(nil)
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ecb_rates'
+require 'rspec'
+
+# source url:
+#
+# http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
+def load_today_fixture
+  File.open('spec/fixtures/eurofxref-daily.xml')
+end
+
+# source url:
+#
+# http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
+def load_history_fixture
+  File.open('spec/fixtures/eurofxref-hist-90d.xml')
+end
+
+# source url:
+#
+# https://www.ecb.europa.eu/stats/eurofxref/eurofxref-sdmx.xml
+def load_full_history_fixture
+  File.open('spec/fixtures/eurofxref-sdmx.xml')
+end
